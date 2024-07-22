d5a05ef7c78bed963a846e29fe0952b6ba14f917
pranava007 <pranavamuthu000@gmail.com>
2024-07-22 15:08:44 +0530

    login complete

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,6 +1,10 @@
 import User from "../Models/userModel.js";
 import { errorHandle } from "../Utils/Error.js";
 import bcryptjs from "bcryptjs";
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const registerUser = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -20,3 +24,35 @@ export const registerUser = async (req, res, next) => {
         next(error);
     }
 }
+
+export const logingUser = async (req,res,next) =>{
+    
+    const {email,password} = req.body;
+    if(!email || !password || email === "" || password === ""){
+        return next(errorHandle(4000,'All the Fields Are Required'))
+    }
+
+    try {
+        
+        const userDetails = await User.findOne({email})
+
+        const userPassword = bcryptjs.compareSync(password,userDetails.password)
+
+        if(!userDetails || !userPassword){
+            return next(errorHandle(400,'Invalid Credentials'))
+        }
+      const token = jwt.sign({id:userDetails._id},process.env.JWT_SECRET_KEY);
+
+      // password hidden not store db
+      const {password:passkey,...rest} = userDetails._doc;
+
+      res.status(200).cookie('access_Token',token,{
+        httpOnly:true,
+      }).json({message:'User LoggedIn Successfully',data:rest})
+
+    } catch (error) {
+        next(error)
+    }
+
+}
+
--- Routers/authRouter.js
+++ Routers/authRouter.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { registerUser } from "../Controllers/userController.js";
+import { logingUser, registerUser } from "../Controllers/userController.js";
 
 const authRoute = express.Router();
 
 authRoute.post('/register-user', registerUser);
+authRoute.post('/login-user',logingUser)
 
 export default authRoute;
