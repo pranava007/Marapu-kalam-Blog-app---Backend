90de0747ba42e9c3ab4b7139b94af33c13913039
pranava007 <pranavamuthu000@gmail.com>
2024-07-30 19:59:35 +0530

    TOKEN

--- Middleware/verfiyToken.js
+++ Middleware/verfiyToken.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 export const verfiyToken = (req,res,next)=>{
   
-    const token = req.headers.authorization;
+    const token = req.headers.token;
     console.log(token);
     if (!token) {
       return next(errorHandle(401, 'Unauthorized Access'));
